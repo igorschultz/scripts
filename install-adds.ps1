ca5d58882c2b022b418d897c0e6d516126eb5b24
Igor Schultz <igorschul@gmail.com>
2024-03-18 15:09:34 -0300

    remove install adds command

--- install-adds.ps1
+++ install-adds.ps1
@@ -1,3 +1,1 @@
-Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-Start-Sleep -Seconds (2 * 60)  # 10 minutes * 60 seconds
 Install-ADDSForest -DomainName igorlab.local -DomainNetbiosName igorlab -SafeModeAdministratorPassword (ConvertTo-SecureString -String 'Trend@123' -AsPlainText -Force) -Force
